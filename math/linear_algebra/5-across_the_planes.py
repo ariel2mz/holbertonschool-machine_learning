e7b3744e1556713e82cf2d586a5a6c64eb12429f
ariel <ariel@gmail.com>
2025-04-02 19:14:47 +0000

    sadsadsa

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -16,14 +16,14 @@ def add_matrices2D(mat1, mat2):
     Returns:
         list:
     """
-    width = len(mat1)
-    height = len(mat1[0])
-    if width != len(mat2):
+    height = len(mat1)
+    width = len(mat1[0])
+    if height != len(mat2):
         return None
-    if height != len(mat2[0]):
+    if width != len(mat2[0]):
         return None
-    nueva = [[0] * height for _ in range(width)]
-    for i in range(height):
-        for j in range(width):
+    nueva = [[0] * width for _ in range(height)]
+    for i in range(width):
+        for j in range(height):
             nueva[i][j] = mat1[i][j] + mat2[i][j]
     return nueva
