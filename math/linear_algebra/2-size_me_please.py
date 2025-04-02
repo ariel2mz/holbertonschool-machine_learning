d899ee38772daddf6391b4c4068810c2e9a44fbe
ariel <ariel@gmail.com>
2025-04-02 18:23:24 +0000

    sadadsa

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+# adasdasdasdasd
 def matrix_shape(matrix):
+    # sadsadsadasdsa
     size = [len(matrix)]
-    if len(matrix) > 0 and isinstance(matrix[0], list):  
+    if len(matrix) > 0 and isinstance(matrix[0], list):
         size.append(len(matrix[0]))
         if len(matrix[0]) > 0 and isinstance(matrix[0][0], list):
             size.append(len(matrix[0][0]))
