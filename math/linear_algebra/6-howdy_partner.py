f070f31f58ea405f8cd6d5fafae31a50a71723f2
ariel <ariel@gmail.com>
2025-04-02 19:32:39 +0000

    jkjjkjk

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+
+ASDSADSADASDSA
+
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Determines the shape (dimensions) of a given matrix.
+
+    Args:
+        matrix (list): A nested list representing the matrix.
+
+    Returns:
+        list:
+    """
+    
+    lenarr1 = len(arr1)
+    lenarr2 = len(arr2)
+    nuevo = arr1 + arr2
+    return nuevo
