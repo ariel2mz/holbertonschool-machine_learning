2b7209c36d16f5a0810f3ed4c58dd4463ad5f9f7
ariel <ariel@gmail.com>
2025-04-02 19:19:30 +0000

    sadsa

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
 """
-sadsadsadsadsaddsa
+
+ASDSADSADASDSA
+
 """
 
 
-def matrix_addition(mat1, mat2):
+def add_matrices2D(mat1, mat2):
     """
-    Adds two matrices element-wise.
+    Determines the shape (dimensions) of a given matrix.
 
     Args:
-        mat1 (list): The first matrix.
-        mat2 (list): The second matrix.
+        matrix (list): A nested list representing the matrix.
 
     Returns:
-        list: A new matrix that is the result of adding mat1 and mat2 element-wise.
-              Returns None if the matrices do not have the same dimensions.
+        list:
     """
-    rows = len(mat1)
-    cols = len(mat1[0])
-    if rows != len(mat2) or cols != len(mat2[0]):
-        return None 
-
-    result = [[0] * cols for _ in range(rows)]
-
-    for i in range(rows):
-        for j in range(cols):
-            result[i][j] = mat1[i][j] + mat2[i][j]
-
-    return result
+    width = len(mat1)
+    height = len(mat1[0])
+    if width != len(mat2):
+        return None
+    if height != len(mat2[0]):
+        return None
+    nueva = [[0] * height for _ in range(width)]
+    for i in range(height):
+        for j in range(width):
+            nueva[i][j] = mat1[i][j] + mat2[i][j]
+    return nueva
