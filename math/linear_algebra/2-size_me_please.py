c71d9af1dbe6ab0b4e8ff93d640c2246d2a04909
ariel <ariel@gmail.com>
2025-04-02 18:25:12 +0000

    asdasda

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-# adasdasdasdasd
 def matrix_shape(matrix):
-    # sadsadsadasdsa
+    """
+    Determines the shape (dimensions) of a given matrix.
+
+    Args:
+        matrix (list): A nested list representing the matrix.
+
+    Returns:
+        list: A list of integers representing the size of each dimension.
+              - [rows] for a 1D list.
+              - [rows, cols] for a 2D matrix.
+              - [rows, cols, depth] for a 3D matrix.
+              and more for a 4d
+    """
     size = [len(matrix)]
     if len(matrix) > 0 and isinstance(matrix[0], list):
         size.append(len(matrix[0]))
