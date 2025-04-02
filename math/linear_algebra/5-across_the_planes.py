faaf2cc9ecce23c9d22a308ae78ca4530ad25d05
ariel <ariel@gmail.com>
2025-04-02 19:10:06 +0000

    assdsadsa

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+
+ASDSADSADASDSA
+
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Determines the shape (dimensions) of a given matrix.
+
+    Args:
+        matrix (list): A nested list representing the matrix.
+
+    Returns:
+        list:
+    """
+    width = len(mat1)
+    height = len(mat1[0])
+    if width != len(mat2):
+        return None
+    if height != len(mat2[0]):
+        return None
+    nueva = [[0] * height for _ in range(width)]
+    for i in range(height):
+        for j in range(width):
+            nueva[i][j] = mat1[i][j] + mat2[i][j]
+    return nueva
