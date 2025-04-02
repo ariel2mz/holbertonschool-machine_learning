64062b0ad8cab3fb95cd46c3476ab53e9880c783
ariel <ariel@gmail.com>
2025-04-02 18:15:58 +0000

    klkl

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
 def matrix_shape(matrix):
-    size = []
-    size.append(len(matrix))
-    size.append(len(matrix[]))
-    size.append(len(matrix[][]))
+    size = [len(matrix)]
+    if len(matrix) > 0 and isinstance(matrix[0], list):  
+        size.append(len(matrix[0]))
+        if len(matrix[0]) > 0 and isinstance(matrix[0][0], list):
+            size.append(len(matrix[0][0]))
+    return size
