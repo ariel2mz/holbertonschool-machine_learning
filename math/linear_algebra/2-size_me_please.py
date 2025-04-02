ce61a7dc5a2f11dd41c522169fd9fc668c5d5cd8
ariel <ariel@gmail.com>
2025-04-02 18:12:11 +0000

    sadsadsa

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,5 +2,5 @@
 def matrix_shape(matrix):
     size = []
     size.append(len(matrix))
-    size.append(len(matrix[0]))
-    size.append(len(matrix[0][0]))
+    size.append(len(matrix[]))
+    size.append(len(matrix[][]))
