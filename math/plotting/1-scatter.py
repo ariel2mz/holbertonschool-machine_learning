d5b320febb21e48f12f52b2597460104327728b3
ariel <ariel@gmail.com>
2025-04-08 09:51:22 -0300

    sadsa

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -10,7 +10,7 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
     
-    plt.scatter(x, y, s=1, color='magenta')
+    plt.scatter(x, y, color='magenta')
     
     plt.xlabel("Height (in)")
     plt.ylabel("Weight (lbs)")
