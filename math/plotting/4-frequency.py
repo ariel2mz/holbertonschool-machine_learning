1e164e214e476c2faf8daa4846c825b51fae91a8
ariel <ariel@gmail.com>
2025-04-08 10:41:55 -0300

    sadsadsa

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -27,8 +27,7 @@ def frequency():
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.title("Project A")
-    plt.ylim(0,30)
-    plt.xlim(0,100)
+    plt.ylim(0, 30)
+    plt.xlim(0, 100)
     plt.xticks(np.arange(0, 101, 10))
     plt.show()
-
