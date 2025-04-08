338e1df1b3ff59dc19028047604d392a4346ce00
ariel <ariel@gmail.com>
2025-04-08 09:49:06 -0300

    sadsadsdsa

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -3,16 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def scatter():
-
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
     plt.figure(figsize=(6.4, 4.8))
-
+    
+    plt.scatter(x, y, s=1, color='magenta')
+    
     plt.xlabel("Height (in)")
     plt.ylabel("Weight (lbs)")
-    plt.table("Men's Height vs Weight")
-    plt.scatter(x, y, s=1, color='magenta')
+    plt.title("Men's Height vs Weight")
+    
     plt.show()
