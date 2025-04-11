9002f77be177820c590b0fed2846e9c24fcf1f6e
ariel <ariel@gmail.com>
2025-04-11 12:39:17 -0300

    sadsadsa

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""
+sadsadsa
+"""
+
 
 def summation_i_squared(n):
+    """
+    Esta descripcion es placeholder no significa nada.
+
+    Args:
+        recibe algun argumento seguramente.
+
+    Returns:
+        algo retorna, y si no retorna algo retorna none
+    """
     i = 1
     numero = 0
     for i in range(1,n):
