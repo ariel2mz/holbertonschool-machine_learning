7ad3e4aa7ce78da93b383b27db7bf78e8555f396
ariel <ariel@gmail.com>
2025-04-12 14:04:35 -0300

    asdsa

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
+"""
+sadsadsa
+"""
+
 
 def bars():
+    """
+    Esta descripcion es placeholder no significa nada.
+
+    Args:
+        recibe algun argumento seguramente.
+
+    Returns:
+        algo retorna, y si no retorna algo retorna none
+    """
+
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
@@ -15,7 +29,9 @@ def bars():
     bottom = np.zeros(len(persons))
 
     for i in range(fruit.shape[0]):
-        plt.bar(persons, fruit[i], width, bottom=bottom, color=colors[i], label=labels[i])
+        plt.bar(persons, fruit[i],
+                 width, bottom=bottom, 
+                 color=colors[i], label=labels[i])
         bottom += fruit[i]
 
     plt.ylabel("Quantity of Fruit")
@@ -23,5 +39,5 @@ def bars():
     plt.yticks(np.arange(0, 81, 10))
     plt.title("Number of Fruit per Person")
     plt.legend()
-    
+
     plt.show()
