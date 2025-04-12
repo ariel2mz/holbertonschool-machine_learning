b46ab9c099b581af715c57e307055f49cd477a93
ariel <ariel@gmail.com>
2025-04-12 14:02:43 -0300

    asdsa

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -2,24 +2,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
-plt.figure(figsize=(6.4, 4.8))
+def bars():
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    plt.figure(figsize=(6.4, 4.8))
 
-categories = ['Farrah', 'Fred',  'Felicia']
-fruit_labels = ['apples', 'bananas', 'oranges', 'pears']
-colors = ['red', 'yellow', 'orange', 'green']
+    labels = ['apples', 'bananas', 'oranges', 'peaches']
+    colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
+    persons = ['Farrah', 'Fred', 'Felicia']
+    width = 0.5
 
+    bottom = np.zeros(len(persons))
 
-# Plot
-for i in range(4):  # for each fruit type
-    plt.bar(categories, fruit[i], bottom=bottom, label=fruit_labels[i], color=colors[i])
-    bottom += fruit[i]  #
+    for i in range(fruit.shape[0]):
+        plt.bar(persons, fruit[i], width, bottom=bottom, color=colors[i], label=labels[i])
+        bottom += fruit[i]
 
-# Labels and legend
-plt.xlabel('Category')
-plt.ylabel('Quantity of Fruit')
-plt.title('Number of Fruit per Person')
-plt.legend()
-
-plt.show()
+    plt.ylabel("Quantity of Fruit")
+    plt.ylim(0, 80)
+    plt.yticks(np.arange(0, 81, 10))
+    plt.title("Number of Fruit per Person")
+    plt.legend()
+    
+    plt.show()
