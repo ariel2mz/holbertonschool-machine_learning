22e4ff403157eaa4c0276ed3dd67ffde667059b5
ariel <ariel@gmail.com>
2025-04-22 10:08:06 -0300

    asdas

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python3
 import numpy as np
 """
-Decision Tree implementation module.
+Neuron implementation module.
 
-This module contains the implementation of a basic Decision
-for supervised learning. It includes the definition of intern
-and the overall tree structure. The tree supports
-and count the number of nodes or leaf nodes.
+adasdsadsadsadsa asddsadasdsa saddsadsa
+asdasdsadsadsad sadsadsada asdasdsa
 
 Classes:
-    Node: Represents an internal node in the decision tree.
-    Leaf: Represents a leaf node (a terminal node with a value).
-    Decision_Tree: The main decision tree class that manages tree structure.
-
-Methods provided:
-    - max_depth_below: Recursively determines the maximum depth.
-    - count_nodes_below: Recursively counts nodes or leaves.
+    Neuron: neurona
 """
 
 
@@ -24,14 +16,11 @@ class Neuron:
     Represents an internal node in the decision tree.
 
     Attributes:
-        feature (int): The feature index used for splitting.
-        threshold (float): The threshold value for splitting.
-        left_child (Node): The left child node.
-        right_child (Node): The right child node.
-        is_leaf (bool): True if node is a leaf.
-        is_root (bool): True if node is the root.
-        sub_population (array): The data subset at this node.
-        depth (int): The depth of the node in the tree.
+        neuron (self): the neuron
+        nx (nx): numeroooo.
+        W (W): random entre 1 y nx.
+        b (b): dasdasdasdasda
+        a (A): asdasdsadsa dsadsadsa
     """
     def __init__(self, nx):
         if not isinstance(nx, int):
