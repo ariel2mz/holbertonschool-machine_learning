097d30210d3a3fd230183528a522908fd3b98a23
ariel <ariel@gmail.com>
2025-05-01 16:14:32 -0300

    kjjk

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.regularizers import l2
+import tensorflow.keras as K
 """
 el proyecto 2 dla semana uwu
 """
@@ -13,16 +11,16 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     hago un modelo cmo ates pero ahora con keras
     q lo hace mucho mas facil
     """
-    model = Sequential()
+    model = K.Sequential()
 
-    model.add(Dense(layers[0], input_dim=nx, activation=activations[0],
-                    kernel_regularizer=l2(lambtha)))
-    model.add(Dropout(1 - keep_prob))
+    model.add(K.Dense(layers[0], input_dim=nx, activation=activations[0],
+                    kernel_regularizer=K.regularizers.l2(lambtha)))
+    model.add(K.Dropout(1 - keep_prob))
 
     for i in range(1, len(layers)):
-        model.add(Dense(layers[i], activation=activations[i],
-                        kernel_regularizer=l2(lambtha)))
-        model.add(Dropout(1 - keep_prob))
+        model.add(K.Dense(layers[i], activation=activations[i],
+                        kernel_regularizer=K.regularizers.l2(lambtha)))
+        model.add(K.Dropout(1 - keep_prob))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
