ff5b1e619c639fb4e8fb3362ae78ac496ad92f58
ariel <ariel@gmail.com>
2025-05-01 16:42:53 -0300

    sadsadsa

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -9,17 +9,21 @@ sasdsadsa
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     hace un modelo con keras
+    
+    Returns: sadsadsadsa
     """
     model = K.Sequential()
 
-    model.add(K.layers.Dense(layers[0], input_dim=nx, activation=activations[0],
+    model.add(K.layers.Dense(layers[0], input_dim=nx,
+                             activation=activations[0],
                              kernel_regularizer=K.regularizers.l2(lambtha),
                              input_shape=(nx,)))
 
     for i in range(1, len(layers)):
         model.add(K.layers.Dropout(1 - keep_prob))
-        model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                 kernel_regularizer=K.regularizers.l2(lambtha)))
+        model.add(K.layers.Dense(
+            layers[i], activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
