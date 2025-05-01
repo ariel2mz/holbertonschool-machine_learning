d883363d57816acd746ae925d9bcb3402c773e33
ariel <ariel@gmail.com>
2025-05-01 16:25:45 -0300

    sada

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,26 +1,28 @@
-#!/usr/bin/env python3
-
 import tensorflow.keras as K
-"""
-el proyecto 2 dla semana uwu
-"""
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    hago un modelo cmo ates pero ahora con keras
-    q lo hace mucho mas facil
+    Builds a neural network with the Keras library.
+
+    nx: number of input features
+    layers: list containing the number of nodes in each layer
+    activations: list containing activation functions for each layer
+    lambtha: L2 regularization parameter
+    keep_prob: probability that a node will be kept for dropout
+
+    Returns: Keras model
     """
     model = K.Sequential()
 
-    model.add(K.Dense(layers[0], input_dim=nx, activation=activations[0],
-                    kernel_regularizer=K.regularizers.l2(lambtha)))
-    model.add(K.Dropout(1 - keep_prob))
+    model.add(K.layers.Dense(layers[0], input_dim=nx, activation=activations[0],
+                             kernel_regularizer=K.regularizers.l2(lambtha)))
+    model.add(K.layers.Dropout(1 - keep_prob))
 
     for i in range(1, len(layers)):
-        model.add(K.Dense(layers[i], activation=activations[i],
-                        kernel_regularizer=K.regularizers.l2(lambtha)))
-        model.add(K.Dropout(1 - keep_prob))
+        model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                 kernel_regularizer=K.regularizers.l2(lambtha)))
+        model.add(K.layers.Dropout(1 - keep_prob))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
