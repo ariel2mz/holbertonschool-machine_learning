ec23f8332baaf8094305d541444b75a59f9e7bd4
ariel <ariel@gmail.com>
2025-05-01 16:38:13 -0300

    asdsadsa

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -19,7 +19,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
 
     for i in range(1, len(layers)):
         model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                 kernel_regularizer=K.regularizers.l2(lambtha)))
+                                 kernel_regularizer=K.regularizers.l2(lambtha),
+                                 input_shape=(nx,))
         model.add(K.layers.Dropout(1 - keep_prob))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy',
