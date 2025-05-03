59174732bde8144b485cdd17737fd96dee592393
ariel <ariel@gmail.com>
2025-05-03 18:35:04 -0300

    sadsa

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -5,18 +5,19 @@ sadsadsadsa dsadsa
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size,
+                epochs, verbose=True, shuffle=False):
     """
     Trains a model using mini-batch gradient descent.
 
     Args:
         network: The Keras model to train.
         data: numpy.ndarray of shape (m, nx) containing the input data.
-        labels: one-hot numpy.ndarray of shape (m, classes) containing the labels.
+        labels: one-hot numpy.ndarray of shape (ls.
         batch_size: Size of each mini-batch for gradient descent.
         epochs: Number of passes through the data.
         verbose: Boolean to control the verbosity of training output.
-        shuffle: Boolean that determines whether to shuffle the data every epoch.
+        shuffle: Boolean that determines whether tpoch.
 
     Returns:
         History object generated after training the model.
