69c2dd747b6155d0616ceaf1d5f2e53a1ad5af70
ariel <ariel@gmail.com>
2025-05-03 01:03:36 -0300

    sadas

--- supervised_learning/classification/27-deep_neural_network.py
+++ supervised_learning/classification/27-deep_neural_network.py
@@ -26,7 +26,7 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if not isinstance(layers, list) or not all(isinstance(x, int) and x > 0 for x in layers):
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
 
         self.__L = len(layers)
