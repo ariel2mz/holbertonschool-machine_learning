8f8a6ebea2acbb6f6b452a028fca8d3f8c8a4637
ariel <ariel@gmail.com>
2025-05-03 17:25:49 -0300

    mkl

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -2,15 +2,24 @@
 
 import tensorflow.keras as K
 """
-sasdsadsa
+This module builds a neural network using the Functional API in Keras.
 """
-
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
-    hace un modelo con keras
-    Returns: sadsadsadsa
+    Builds a neural network model using the Keras Functional API.
+
+    Parameters:
+    - nx: number of input features
+    - layers: list containing the number of nodes in each layer
+    - activations: list containing activation functions for each layer
+    - lambtha: L2 regularization parameter
+    - keep_prob: probability of keeping a node during dropout
+
+    Returns:
+    - A compiled Keras model.
     """
+    # Input layer
     inputs = K.Input(shape=(nx,))
     x = K.layers.Dense(
         layers[0],
@@ -18,13 +27,19 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         kernel_regularizer=K.regularizers.l2(lambtha)
     )(inputs)
 
+    # Hidden layers with Dropout
     for i in range(1, len(layers)):
-        model.add(K.layers.Dropout(1 - keep_prob))
-        model.add(K.layers.Dense(
-            layers[i], activation=activations[i],
-            kernel_regularizer=K.regularizers.l2(lambtha)))
+        x = K.layers.Dropout(1 - keep_prob)(x)
+        x = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+        )(x)
 
+    # Create the model with the explicit name
     model = K.Model(inputs=inputs, outputs=x, name="model")
+
+    # Compile the model
     model.compile(optimizer='adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
