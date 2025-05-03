5683b6021e62b2d3a0c29109433f62e21889a2e6
ariel <ariel@gmail.com>
2025-05-03 18:42:02 -0300

    asdas

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+sadsadsadsa dsadsa
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent.
+
+    Args:
+        network: The Keras model to train.
+        data: numpy.ndarray of shape (m, nx) containing the input data.
+        labels: one-hot numpy.ndarray of shape (ls.
+        batch_size: Size of each mini-batch for gradient descent.
+        epochs: Number of passes through the data.
+        verbose: Boolean to control the verbosity of training output.
+        shuffle: Boolean that determines whether tpoch.
+
+    Returns:
+        History object generated after training the model.
+    """
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data)
