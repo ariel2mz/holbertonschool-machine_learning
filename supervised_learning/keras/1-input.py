e0d95bc74fc598a5c473b9bd80005491035e0cb0
ariel <ariel@gmail.com>
2025-05-03 17:15:50 -0300

    asdasdsa

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import tensorflow.keras as K
+"""
+sasdsadsa
+"""
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    hace un modelo con keras
+    Returns: sadsadsadsa
+    """
+    model = K.Sequential()
+
+    model.add(K.layers.Dense(layers[0], input_dim=nx,
+                             activation=activations[0],
+                             kernel_regularizer=K.regularizers.l2(lambtha),
+                             input_shape=(nx,)))
+
+    for i in range(1, len(layers)):
+        model.add(K.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dense(
+            layers[i], activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
