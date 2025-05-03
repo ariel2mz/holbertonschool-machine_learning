becc75332daf74b61c1c96c06fd5f7fd87427340
ariel <ariel@gmail.com>
2025-05-03 18:29:12 -0300

    sadsadsa

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-
-import tensorflow.keras as K
 """
 This module builds a neural network using the Functional API in Keras.
 """
+import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
@@ -11,10 +10,13 @@ def optimize_model(network, alpha, beta1, beta2):
     Builds a neural network model using the Keras Functional API.
 
     Parameters:
-    - network: asdasdsa dsasa
-    - alpha: asdsadsa asdas
-    - beta1: asdsad sadsa
-    - beta2: asdsadsad asddsa
+    - network (keras.Model):sadsadsa sdadsa.
+    - alpha (float): asdsadsadsa sdadsad.
+    - beta1 (float): sadsadsad sadsadsa.
+    - beta2 (float): sadasd sadsadsa dsa.
+
+    Returns:
+    - None: nada.
     """
     adam = K.optimizers.Adam(alpha, beta1, beta2)
     network.compile(optimizer=adam,
--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+sadsadsadsa sadsa
+"""
 import tensorflow.keras as K
 
 
