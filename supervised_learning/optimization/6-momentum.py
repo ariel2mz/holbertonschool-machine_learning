07e2b3b56646af67b862bc2c2c3136e7b7cbcb7a
ariel <ariel@gmail.com>
2025-05-10 19:27:38 -0300

    asdsa

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+no se amigo recien empiezo el codigo
+"""
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    alpha: learnin rate
+    beta1: cuanta velocidad conserva
+    """
+    optimizer = tf.optimizers.SGD(learning_rate=alpha, momentum=beta1)
+    return optimizer
