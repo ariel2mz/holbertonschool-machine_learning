d2029992da377a93169c9ff9be659903169447cb
ariel <ariel@gmail.com>
2025-05-17 19:36:28 -0300

    asas

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+no se amigo recien empiezo el codigo
+"""
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """
+    Calculates the total cost of a neural network with L2 regularization.
+    
+    Parameters:
+    cost -- tensor containing the cost of the network without L2 regularization
+    model -- Keras model that includes layers with L2 regularization
+    
+    Returns:
+    A tensor containing the total cost for each layer of the network, accounting for L2 regularization
+    """
+
+    l2_cost = tf.zeros_like(cost)
+    for layer in model.layers:
+        l2 = layer.kernel_regularizer.l2
+        weights = layer.kernel
+        l2_cost += l2 * tf.nn.l2_loss(weights)
+    total_cost = cost + l2_cost
+
+    return total_cost
