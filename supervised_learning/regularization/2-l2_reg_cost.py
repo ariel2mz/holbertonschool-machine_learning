456f1bc1ca5205977eb7185bc11b9f7f990494f2
ariel <ariel@gmail.com>
2025-05-17 19:46:48 -0300

    asdas

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -10,13 +10,14 @@ def l2_reg_cost(cost, model):
     Calculates the cost of a neural network with L2 regularization.
     Parameters:
     cost (tensor): The cost of the network without L2 regularization.
-    model (tf.keras.Model): The Keras model that includes layers with L2 regularization.
+    model (tf.keras.Model): The Keras model that includes layers with L2.
     Returns:
     el fuckin costo tio
     """
     l2_loss = tf.zeros_like(cost)
     for layer in model.layers:
-        if hasattr(layer, 'kernel_regularizer') and layer.kernel_regularizer is not None:
-            l2_loss += layer.kernel_regularizer(layer.kernel)
+        if hasattr(layer, 'kernel_regularizer'):
+            if layer.kernel_regularizer is not None:
+                l2_loss += layer.kernel_regularizer(layer.kernel)
 
     return cost + l2_loss
