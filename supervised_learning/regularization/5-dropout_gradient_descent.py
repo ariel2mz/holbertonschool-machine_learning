a1429836f572e062e7bf2f35c29bddfc5655d0bb
ariel <ariel@gmail.com>
2025-05-18 18:21:08 -0300

    sadsa

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -13,14 +13,14 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     grds = {}
     m = Y.shape[1]
     A_output = cache['A' + str(L)]
-    dZ = A_output - Y 
+    dZ = A_output - Y
 
     for i in range(L, 0, -1):
         A_prev = cache['A' + str(i-1)]
         grds['db' + str(i)] = np.sum(dZ, axis=1, keepdims=True) / m
         grds['dW' + str(i)] = np.dot(dZ, A_prev.T) / m
-
-        if i != 1: 
+    
+        if i != 1:
             dA = np.matmul(weights['W' + str(i)].T, dZ)
             dA *= cache['D' + str(i-1)]
             dA /= keep_prob
