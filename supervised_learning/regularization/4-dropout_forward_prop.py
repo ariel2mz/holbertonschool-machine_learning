73dd717fec064a96c8316f8ddc2a89cd516b50f9
ariel <ariel@gmail.com>
2025-05-18 17:38:47 -0300

    sadas

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python3
-"""
-no se amigo recien empiezo el codigo
-"""
 import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
-    Conducts forward propagation using Dropout.
-
+    Conduct forward propagation using Dropout.
+    
     Parameters:
-    X (numpy.ndarray): Input data of shape (nx, m)
-    weights (dict): Dictionary containing weights and biases of the neural network
-    L (int): Number of layers in the network
-    keep_prob (float): Probability that a node will be kept
+    - X: input data (nx, m)
+    - weights: dictionary of weights and biases
+    - L: number of layers
+    - keep_prob: probability of keeping a neuron active
 
     Returns:
-    dict: A dictionary containing the outputs of each layer and the dropout mask used on each layer
+    - cache: dictionary with layer activations and dropout masks
     """
     cache = {}
-    A = X
-    m = X.shape[1]
+    cache['A0'] = X
 
-    for l in range(1, L + 1):
-        W = weights['W' + str(l)]
-        b = weights['b' + str(l)]
+    for i in range(1, L + 1):
+        W = weights['W' + str(i)]
+        b = weights['b' + str(i)]
+        A_prev = cache['A' + str(i - 1)]
 
-        Z = np.dot(W, A) + b
-        if l < L:
+        Z = np.matmul(W, A_prev) + b
+
+        if i == L:
+            # a la ultima layer se le hace softmax
+            """
+            La función softmax transforma un vector de valores reales
+            (por ejemplo, las salidas de la última capa de una red neuronal)
+            en una distribución de probabilidad. Es decir, convierte
+            esos valores en números entre 0 y 1 que suman 1 y pueden interpretarse como
+            probabilidades.
+            atentamente: chat gepete
+            """
+            A = np.exp(Z - np.max(Z, axis=0, keepdims=True))
+            A = A / np.sum(A, axis=0, keepdims=True)
+        else:
             A = np.tanh(Z)
             D = np.random.rand(*A.shape) < keep_prob
             A *= D
             A /= keep_prob
-            cache['D' + str(l)] = D
-        else:
-            exp_Z = np.exp(Z - np.max(Z, axis=0, keepdims=True))
-            A = exp_Z / np.sum(exp_Z, axis=0, keepdims=True)
+            cache['D' + str(i)] = D
 
-        cache['A' + str(l)] = A
+        cache['A' + str(i)] = A
 
     return cache
