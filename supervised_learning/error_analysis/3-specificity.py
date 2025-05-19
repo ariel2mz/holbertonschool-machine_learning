52f331996556423bc0c3f6871a554554636830bf
ariel <ariel@gmail.com>
2025-05-18 21:09:02 -0300

    asdasdsa

---
+++ supervised_learning/error_analysis/3-specificity.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def specificity(confusion):
+    """
+    Calculates the specificity for each class in a confusion matrix.
+
+    Parameters:
+    - confusion: np.ndarray of shape (classes, classes)
+
+    Returns:
+    - np.ndarray of shape (classes,) containing the specificity of each class
+    """
+    classes = confusion.shape[0]
+    total = np.sum(confusion)
+    lista = np.zeros(classes)
+
+    for i in range(classes):
+        corpos = confusion[i, i]
+        falsopos = np.sum(confusion[:, i]) - corpos
+        falsoneg = np.sum(confusion[i, :]) - corpos
+        corneg = total - (corpos + falsopos + falsoneg)
+
+        if (corneg + falsopos) > 0:
+            lista[i] = corneg / (corneg + falsopos)
+        else:
+            lista[i] = 0
+
+    return lista
