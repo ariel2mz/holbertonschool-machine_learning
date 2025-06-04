de4301a195330c4312ad3ad2e4161a1ed2b685f3
ariel <ariel@gmail.com>
2025-06-04 15:26:05 -0300

    asdsadas

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -24,21 +24,21 @@ def inception_network():
     X = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
                               padding='same')(X)
 
-    X = inception_block(X, (64, 96, 128, 16, 32, 32))
-    X = inception_block(X, (128, 128, 192, 32, 96, 64))
+    X = inception_block(X, [64, 96, 128, 16, 32, 32])
+    X = inception_block(X, [128, 128, 192, 32, 96, 64])
     X = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
                               padding='same')(X)
 
-    X = inception_block(X, (192, 96, 208, 16, 48, 64))
-    X = inception_block(X, (160, 112, 224, 24, 64, 64))
-    X = inception_block(X, (128, 128, 256, 24, 64, 64))
-    X = inception_block(X, (112, 144, 288, 32, 64, 64))
-    X = inception_block(X, (256, 160, 320, 32, 128, 128))
+    X = inception_block(X, [192, 96, 208, 16, 48, 64])
+    X = inception_block(X, [160, 112, 224, 24, 64, 64])
+    X = inception_block(X, [128, 128, 256, 24, 64, 64])
+    X = inception_block(X, [112, 144, 288, 32, 64, 64])
+    X = inception_block(X, [256, 160, 320, 32, 128, 128])
     X = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2),
                               padding='same')(X)
 
-    X = inception_block(X, (256, 160, 320, 32, 128, 128))
-    X = inception_block(X, (384, 192, 384, 48, 128, 128))
+    X = inception_block(X, [256, 160, 320, 32, 128, 128])
+    X = inception_block(X, [384, 192, 384, 48, 128, 128])
 
     X = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1),
                                   padding='valid')(X)
