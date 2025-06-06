339a7cc0009319030bd69afa8de85e9e4c118e27
ariel <ariel@gmail.com>
2025-06-06 18:30:55 -0300

    asdasdas

---
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""sadsadsads"""
+import tensorflow.keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """
+    sadfdsdadfgf sadsad
+    """
+    F11, F3, F12 = filters
+    init = K.initializers.HeNormal(seed=0)
+
+    camA = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(s, s),
+                        padding='valid', kernel_initializer=init)(A_prev)
+    camA = K.layers.BatchNormalization(axis=3)(camA)
+    camA = K.layers.Activation('relu')(camA)
+
+    camA = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1),
+                        padding='same', kernel_initializer=init)(camA)
+    camA = K.layers.BatchNormalization(axis=3)(camA)
+    camA = K.layers.Activation('relu')(camA)
+
+    camA = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
+                        padding='valid', kernel_initializer=init)(camA)
+    camA = K.layers.BatchNormalization(axis=3)(camA)
+
+    camB = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(s, s),
+                               padding='valid', kernel_initializer=init)(A_prev)
+    camB = K.layers.BatchNormalization(axis=3)(camB)
+
+    final = K.layers.Add()([camA, camB])
+    final = K.layers.Activation('relu')(final)
+
+    return final
