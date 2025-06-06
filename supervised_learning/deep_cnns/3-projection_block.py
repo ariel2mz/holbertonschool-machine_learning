80a61adc74fda2cdb117c10cf51f55c8220fc401
ariel <ariel@gmail.com>
2025-06-06 18:37:09 -0300

    asdsa

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -11,21 +11,21 @@ def projection_block(A_prev, filters, s=2):
     init = K.initializers.HeNormal(seed=0)
 
     camA = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(s, s),
-                        padding='valid', kernel_initializer=init)(A_prev)
+                           padding='valid', kernel_initializer=init)(A_prev)
     camA = K.layers.BatchNormalization(axis=3)(camA)
     camA = K.layers.Activation('relu')(camA)
 
     camA = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1),
-                        padding='same', kernel_initializer=init)(camA)
+                           padding='same', kernel_initializer=init)(camA)
     camA = K.layers.BatchNormalization(axis=3)(camA)
     camA = K.layers.Activation('relu')(camA)
 
     camA = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
-                        padding='valid', kernel_initializer=init)(camA)
+                           padding='valid', kernel_initializer=init)(camA)
     camA = K.layers.BatchNormalization(axis=3)(camA)
 
     camB = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(s, s),
-                               padding='valid', kernel_initializer=init)(A_prev)
+                           padding='valid', kernel_initializer=init)(A_prev)
     camB = K.layers.BatchNormalization(axis=3)(camB)
 
     final = K.layers.Add()([camA, camB])
