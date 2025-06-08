f6270a7475b177799f2908a9d74299836c2e7610
ariel <ariel@gmail.com>
2025-06-08 19:04:15 -0300

    asdasdsa

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""sadadasdas"""
+from tensorflow import keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """
+    s  adadasd 
+    asdsadsa
+    """
+    init = K.initializers.HeNormal(seed=0)
+
+    for i in range(layers):
+        bn1 = K.layers.BatchNormalization(axis=-1)(X)
+        relu1 = K.layers.Activation('relu')(bn1)
+        conv1 = K.layers.Conv2D(
+            filters=4 * growth_rate,
+            kernel_size=1,
+            padding='same',
+            kernel_initializer=init
+        )(relu1)
+
+        bn2 = K.layers.BatchNormalization(axis=-1)(conv1)
+        relu2 = K.layers.Activation('relu')(bn2)
+        conv2 = K.layers.Conv2D(
+            filters=growth_rate,
+            kernel_size=3,
+            padding='same',
+            kernel_initializer=init
+        )(relu2)
+
+        X = K.layers.Concatenate(axis=-1)([X, conv2])
+        nb_filters += growth_rate
+
+    return X, nb_filters
