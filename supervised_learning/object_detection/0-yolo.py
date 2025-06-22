b2d1e25cada0d3ea9ee8cb075ae3ba43eb1c8b35
ariel <ariel@gmail.com>
2025-06-22 17:40:40 -0300

    sada

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+
+K = tf.keras
+
+class Yolo:
+    """asdsadasdasdsa"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        sadasdasdsa
+
+        Parameters:
+        - model_path: sada
+        - classes_path: adsadas
+        - class_t: asdasds
+        - nms_t: sadasds
+        - anchors:asdasdasda
+        """
+        self.model = K.models.load_model(model_path)
+
+        with open(classes_path, 'r') as f:
+            self.class_names = [line.strip() for line in f.readlines()]
+
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
