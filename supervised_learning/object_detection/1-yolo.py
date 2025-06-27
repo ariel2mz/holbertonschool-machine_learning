86dc60d6a85702c6a92234fd2e7d16899662b92a
ariel <ariel@gmail.com>
2025-06-27 19:06:04 -0300

    sadsadas

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -40,15 +40,17 @@ class Yolo:
             Tuple of (boxes, box_confidences, box_class_probs)
         """
         boxes = []
-        conf = []
-        prob = []
+        boxconf = []  # Changed from 'conf' to avoid conflict
+        boxprobs = []  # Changed from 'prob' to avoid potential conflicts
         image_h, image_w = image_size
 
+        # Get model input shape from the model itself
         self.input_height, self.input_width = self.model.input.shape[1:3]
 
         for i, output in enumerate(outputs):
             grid_h, grid_w, anchors, _ = output.shape
-
+            
+            # Create grid coordinates
             grid_y = np.arange(grid_h).reshape(grid_h, 1, 1)
             grid_x = np.arange(grid_w).reshape(1, grid_w, 1)
 
@@ -56,17 +58,20 @@ class Yolo:
             ty = output[..., 1]
             tw = output[..., 2]
             th = output[..., 3]
-            boxconf = output[..., 4]
-            class_probs = output[..., 5:]
+            conf = output[..., 4]  # Renamed to avoid conflict
+            probs = output[..., 5:]  # Renamed for clarity
 
+            # Reshape anchors to match output dimensions
             anchor_w = self.anchors[i, :, 0].reshape(1, 1, anchors)
             anchor_h = self.anchors[i, :, 1].reshape(1, 1, anchors)
 
+            # Calculate box coordinates
             bx = (1 / (1 + np.exp(-tx)) + grid_x) / grid_w
             by = (1 / (1 + np.exp(-ty)) + grid_y) / grid_h
             bw = (np.exp(tw) * anchor_w) / self.input_width
             bh = (np.exp(th) * anchor_h) / self.input_height
 
+            # Convert to image coordinates
             x1 = (bx - bw / 2) * image_w
             y1 = (by - bh / 2) * image_h
             x2 = (bx + bw / 2) * image_w
@@ -78,8 +83,9 @@ class Yolo:
                 x2[..., np.newaxis], y2[..., np.newaxis]], axis=-1
             )
             boxes.append(box)
-
+            
+            # Apply sigmoid and store results
             boxconf.append(1 / (1 + np.exp(-conf)))
-            prob.append(1 / (1 + np.exp(-class_probs)))
+            boxprobs.append(1 / (1 + np.exp(-probs)))
 
-        return boxes, conf, prob
+        return boxes, boxconf, boxprobs
