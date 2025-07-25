1ba57b99adb4a9a0489b2016c1dd3786f84db0f9
ariel <ariel@gmail.com>
2025-07-25 17:51:20 -0300

    dfghj

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -4,6 +4,7 @@ asdsaddas
 """
 import numpy as np
 
+
 def mean_cov(X):
     """
     sadsadas
@@ -12,9 +13,9 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     if X.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
-    
+
     mean = np.mean(X, axis=0, keepdims=True)
-    
+
     """
     formula para obtener el cov
     """
