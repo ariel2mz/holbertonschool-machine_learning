dff3e306e6f14bbd3149cabf79f5a8f20ed10722
ariel <ariel@gmail.com>
2025-07-25 19:38:00 -0300

    dfs

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+asdsaddas
+"""
+import numpy as np
+fact = np.math.factorial
+
+
+def likelihood(x, n, P):
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+    if x < 0:
+        raise ValueError("x must be an integer that is greater than or equal to 0")
+    if x > n:
+        raise  ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    
+    bicoef = fact(n) / (fact(x) * fact(n - x))
+    lihood = bicoef * (P ** x) * ((1 - P) ** (n - x))
+
+    return lihood
