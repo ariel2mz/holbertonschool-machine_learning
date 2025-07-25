9aa6bde82544a9283159e8ea211fdf7d68270ea7
ariel <ariel@gmail.com>
2025-07-25 17:22:00 -0300

    sdsds

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+asdsaddas
+"""
+import numpy as np
+
+def mean_cov(X):
+    """
+    sadsadas
+    """
+    if not isinstance(X, np.ndarray) or len(X) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    
+    mean = np.mean(X, axis=0, keepdims=True)
+    
+    """
+    formula para obtener el cov
+    """
+    Xcent = X - mean
+    cov = (Xcent.T @ Xcent) / (X.shape[0] - 1)
+
+    return mean, cov
---
+++ math/multivariate_prob/README.md
@@ -0,0 +1,1 @@
+def mean_cov(X):
