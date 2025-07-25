a9bd3ecd8d07e2a2f56efad5590a175a373fedaa
ariel <ariel@gmail.com>
2025-07-25 20:36:12 -0300

    sds

---
+++ math/bayesian_prob/3-posterior.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""
+asdsaddas
+"""
+import numpy as np
+fact = np.math.factorial
+ve = "x must be an integer that is greater than or equal to 0"
+
+
+def likelihood(x, n, P):
+    """
+    ghjklkjhgfdfghjkl
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(ve)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    bicoef = fact(n) / (fact(x) * fact(n - x))
+    lihood = bicoef * (P ** x) * ((1 - P) ** (n - x))
+
+    return lihood
+
+
+def intersection(x, n, P, Pr):
+    """
+    asdgfdsas
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(ve)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(Pr.sum(), 1):
+        raise ValueError("Pr must sum to 1")
+
+    lihood = likelihood(x, n, P)
+    inter = lihood * Pr
+    return inter
+
+
+def marginal(x, n, P, Pr):
+    """
+    sdsfsd
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(ve)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(Pr.sum(), 1):
+        raise ValueError("Pr must sum to 1")
+
+    inter = intersection(x, n, P, Pr)
+    marginal = np.sum(inter)
+
+    return marginal
+
+
+def posterior(x, n, P, Pr):
+    """
+    sdfgdfsd
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(ve)
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not np.all((P >= 0) & (P <= 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if not isinstance(Pr, np.ndarray) or P.shape != Pr.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if not np.all((Pr >= 0) & (Pr <= 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(Pr.sum(), 1):
+        raise ValueError("Pr must sum to 1")
+
+    inter = intersection(x, n, P, Pr)
+    marg = marginal(x, n, P, Pr)
+    posterior = inter / marg
+    return posterior
