ef83ffd4f4769cc4ae9a8e4d2be8f55981a3a621
ariel <ariel@gmail.com>
2025-07-27 20:04:09 -0300

    kkl

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -7,13 +7,11 @@ def pca(X, var=0.95):
     """
     sadadsada
     """
-    X_centered = X - np.mean(X, axis=0)
-    no, sv, rsv = np.linalg.svd(X_centered, full_matrices=False)
-
-    expvar = (sv ** 2) / (X_centered.shape[0] - 1)
-    totvar = np.sum(expvar)
-    cumvar = np.cumsum(expvar) / totvar
-
-    n = np.argmax(cumvar >= var) + 1
-
-    return rsv[:n].T
+    
+    Lval, Sval, Rval = np.linalg.svd(
+        X, full_matrices=False)
+    
+    cumvar = np.cumsum(Sval) / np.sum(Sval)
+    comps = np.argmax(cumvar >= var) + 1
+    
+    return Rval[:comps].T
