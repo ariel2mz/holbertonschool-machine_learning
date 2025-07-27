720000ea3531bbea4602e073f1d3ad1876428ec9
ariel <ariel@gmail.com>
2025-07-27 20:05:01 -0300

    sadas

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -7,11 +7,11 @@ def pca(X, var=0.95):
     """
     sadadsada
     """
-    
+
     Lval, Sval, Rval = np.linalg.svd(
         X, full_matrices=False)
-    
+
     cumvar = np.cumsum(Sval) / np.sum(Sval)
     comps = np.argmax(cumvar >= var) + 1
-    
+
     return Rval[:comps].T
