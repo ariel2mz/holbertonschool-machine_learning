6990fedb3b80d7dfa710603de27f7dceb58b0236
ariel <ariel@gmail.com>
2025-07-27 19:45:52 -0300

    sdsds

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -5,21 +5,15 @@ import numpy as np
 
 def pca(X, var=0.95):
     """
-    dsadasdsa
+    dasdsadsa
     """
-
     X_centered = X - np.mean(X, axis=0)
     covm = np.cov(X_centered, rowvar=False)
     eivals, eivecs = np.linalg.eigh(covm)
     sortedidx = np.argsort(eivals)[::-1]
     eivals = eivals[sortedidx]
     eivecs = eivecs[:, sortedidx]
-    # This step helps match the expected output signs
-    for i in range(eivecs.shape[1]):
-        if eivecs[0, i] < 0:
-            eivecs[:, i] *= -1
     cuvar = np.cumsum(eivals) / np.sum(eivals)
     nd = np.argmax(cuvar >= var) + 1
-    W = eivecs[:, :nd]
 
-    return W
+    return eivecs[:, :nd]
