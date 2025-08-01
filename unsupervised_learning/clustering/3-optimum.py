eb9cd1ef9987f95ff257fc64c903d32b125b2fa9
ariel <ariel@gmail.com>
2025-08-01 19:28:13 -0300

    kjlklk

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -13,7 +13,7 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     if (not isinstance(X, np.ndarray) or len(X.shape) != 2 or
         not isinstance(kmin, int) or kmin <= 0 or
         (kmax is not None and (not isinstance(kmax, int) or kmax <= 0)) or
-        not isinstance(iterations, int) or iterations <= 0):
+            not isinstance(iterations, int) or iterations <= 0):
         return None, None
 
     n = X.shape[0]
@@ -33,7 +33,6 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         var = variance(X, cents)
         dvars.append(var)
 
-   # Normalize variances based on the smallest k
     minvar = dvars[0]
     dvdiff = []
     for v in dvars:
