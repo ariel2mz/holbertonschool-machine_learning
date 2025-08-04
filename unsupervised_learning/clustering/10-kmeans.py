406770726997ff922b6c97fcd2bf310befb3df61
ariel <ariel@gmail.com>
2025-08-03 22:26:48 -0300

    sadas

---
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -0,0 +1,10 @@
+import sklearn.cluster
+
+def kmeans(X, k):
+    """
+    sadasdsadsa
+    """
+    model = sklearn.cluster.KMeans(n_clusters=k, random_state=0).fit(X)
+    C = model.cluster_centers_
+    clss = model.labels_
+    return C, clss
