d2a36ab6f9a92f2342b4ed779a617f8544624bb9
ariel <ariel@gmail.com>
2025-08-09 17:58:44 -0300

    jkjjk

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -25,7 +25,7 @@ def regular(P):
     A = np.vstack([P.T - np.eye(n), np.ones(n)])
     b = np.zeros(n + 1)
     b[-1] = 1
-    
+
     try:
         sstate = np.linalg.lstsq(A, b, rcond=None)[0]
         return sstate.reshape(1, n)
