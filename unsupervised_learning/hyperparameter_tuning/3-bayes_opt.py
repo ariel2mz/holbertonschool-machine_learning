e48ac57e37909da628426ce28af69985d753fd3f
ariel <ariel@gmail.com>
2025-08-17 13:27:26 -0300

    sadas

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -8,12 +8,14 @@ class BayesianOptimization:
     """
     dfghjkl
     """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
         dfghljfdfjk
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
-        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.X_s = np.linspace(bounds[0],
+                               bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
