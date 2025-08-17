c2136b801e128c6203920f1da60a2b6435ba1104
ariel <ariel@gmail.com>
2025-08-17 10:23:34 -0300

    sada

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -21,7 +21,8 @@ class GaussianProcess:
         """
         asdasdasdsa
         """
-        dist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        abv = np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        dist = np.sum(X1**2, 1).reshape(-1, 1) + abv
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * dist)
 
     def predict(self, X_s):
