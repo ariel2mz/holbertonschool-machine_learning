8eaa1a7935eb478a3acddbf286a21d420e5b2ef0
ariel <ariel@gmail.com>
2025-09-28 13:27:52 -0300

    asd

---
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+sadsadsadsadsa
+"""
+import tensorflow as tf
+
+
+class RNNEncoder(tf.keras.layers.Layer):
+    """
+    sadasdsad
+    """
+    def __init__(self, vocab, embedding, units, batch):
+        """
+        asdsadsa
+        """
+        super(RNNEncoder, self).__init__()
+        self.batch = batch
+        self.units = units
+
+        self.embedding = tf.keras.layers.Embedding(
+            input_dim=vocab,
+            output_dim=embedding
+        )
+
+        self.gru = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer="glorot_uniform"
+        )
+
+    def initialize_hidden_state(self):
+        """
+        asdsadsadas
+        """
+        return tf.zeros((self.batch, self.units))
+
+    def call(self, x, initial):
+        """
+        dsadsadsadsadsa
+        """
+   
+        x = self.embedding(x)
+
+        outputs, hidden = self.gru(x, initial_state=initial)
+
+        return outputs, hidden
