0560776fe028a71e0a2a69cfc685dc19aa70c93a
ariel <ariel@gmail.com>
2025-10-05 15:48:34 -0300

    sda

--- supervised_learning/transformer_apps/2-dataset.py
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -12,16 +12,17 @@ class Dataset:
         """
         sadasdsadsa
         """
-        train = tfds.load(
+        self.datatrain = tfds.load(
             'ted_hrlr_translate/pt_to_en', split='train',
             as_supervised=True)
-        validate = tfds.load(
+        self.datavalid = tfds.load(
             'ted_hrlr_translate/pt_to_en', split='validation',
             as_supervised=True)
-        self.tokenizerpt, self.tokenizeren = self.tokenizedataset(
-           train)
-        self.datavalid = validate.map(self.tfencode)
-        self.datatrain = train.map(self.tfencode)
+        a, b = self.tokenizedataset(self.datatrain)
+        self.tokenizerpt, self.tokenizeren = a, b
+        
+        self.datatrain = self.datatrain.map(self.tfencode)
+        self.datavalid = self.datavalid.map(self.tfencode)
 
     def tokenizedataset(self, data):
         """
@@ -33,16 +34,12 @@ class Dataset:
             ptsen.append(pt.numpy().decode('utf-8'))
             ensen.append(en.numpy().decode('utf-8'))
         tpt = transformers.AutoTokenizer.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased',
-            clean_up_tokenization_spaces=True, use_fast=True)
+            'neuralmind/bert-base-portuguese-cased')
         ten = transformers.AutoTokenizer.from_pretrained(
-            'bert-base-uncased',
-            clean_up_tokenization_spaces=True, use_fast=True)
+            'bert-base-uncased')
 
-        tpt = tpt.train_new_from_iterator(
-            ptsen, 2**13)
-        ten = ten.train_new_from_iterator(
-            ensen, 2**13)
+        tpt = tpt.train_new_from_iterator(ptsen, 2**13)
+        ten = ten.train_new_from_iterator(ensen, 2**13)
 
         return tpt, ten
 
@@ -50,12 +47,10 @@ class Dataset:
         """
         sadsadsa
         """
-        vsen = self.tokenizeren.vocab_size
         vspt = self.tokenizerpt.vocab_size
-        pttok = self.tokenizerpt.encode(
-            pt.numpy().decode('utf-8'), add_special_tokens=False)
-        entok = self.tokenizeren.encode(
-            en.numpy().decode('utf-8'), add_special_tokens=False)
+        vsen = self.tokenizeren.vocab_size
+        pttok = self.tokenizerpt.encode(pt.numpy().decode('utf-8'))
+        entok = self.tokenizeren.encode(en.numpy().decode('utf-8'))
         pttok = [vspt] + pttok + [vspt + 1]
         entok = [vsen] + entok + [vsen + 1]
 
