ff01f3be359ea088b93d6ab44c2d8b2d42209bf4
ariel <ariel@gmail.com>
2025-11-06 19:43:33 -0300

    afsaf

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -14,19 +14,25 @@ def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
 
         for _ in range(max_steps):
             act = policy(sta)
-            nextstate, rew, ter, trunc, _ = env.step(act)
-            epo.append((sta, rew))
-            sta = nextstate
+            nextsta, rew, ter, trunc, _ = env.step(act)
+            epi.append((sta, rew))
+            sta = nextsta
             if ter or trunc:
                 break
         G = 0
-        epi = np.array(episode, dtype=int)
 
-        for t in range(len(episodes) - 1, -1, -1):
+        for t in range(len(epi) - 1, -1, -1):
             sta, rew = epi[t]
             G = rew + gamma * G
 
-            if sta not in epi[:x, 0]:
+            # Check if this is the first time we see this state in the episode
+            first_visit = True
+            for i in range(t):
+                if epi[i][0] == sta:
+                    first_visit = False
+                    break
+            
+            if first_visit:
                 V[sta] += alpha * (G - V[sta])
 
     return V
