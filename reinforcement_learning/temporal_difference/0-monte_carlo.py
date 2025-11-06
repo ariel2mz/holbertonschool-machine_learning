0ad71948e664b114ecd9794c89c3619ca5c5512e
ariel <ariel@gmail.com>
2025-11-06 19:36:29 -0300

    safsa

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
-"""
-afsfsafasf
-"""
+"""sagasgas"""
 import numpy as np
 
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
+                gamma=0.99):
     """
-    safsafsa
+
     """
-    for ep in range(episodes):
+    for x in range(episodes):
         sta, _ = env.reset()
         epi = []
 
         for _ in range(max_steps):
             act = policy(sta)
-            nextsta, rew, term, trunc, _ = env.step(act)
-            epi.append((sta, rew))
-            sta = nextsta
-            if term or trunc:
+            nextstate, rew, ter, trunc, _ = env.step(act)
+            epo.append((sta, rew))
+            sta = nextstate
+            if ter or trunc:
                 break
-
         G = 0
-        epi = np.array(epi, dtype=int)
+        epi = np.array(episode, dtype=int)
 
-        for t in range(len(epi) - 1, -1, -1):
+        for t in range(len(episodes) - 1, -1, -1):
             sta, rew = epi[t]
             G = rew + gamma * G
-            if sta not in epi[:t, 0]:
-                V[sta] = V[sta] + alpha * (G - V[sta])
+
+            if sta not in epi[:x, 0]:
+                V[sta] += alpha * (G - V[sta])
 
     return V
