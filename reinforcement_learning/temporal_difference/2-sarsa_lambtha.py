8ec6925c7915b5c12e53b86e58e49825fd6fb5f4
ariel <ariel@gmail.com>
2025-11-09 15:46:57 -0300

    saaf

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
-"""sagasgas"""
+"""sarsalambtha"""
 import numpy as np
 
+
+def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
+                  gamma=0.99, epsilon=1, min_epsilon=0.1,
+                  epsilon_decay=0.05):
+    """
+    asgashasgas
+    """
+    E = np.zeros_like(Q)
+    nS, nA = Q.shape
+    
+    for _ in range(episodes):
+        sta, _ = env.reset()
+        E.fill(0)
+        
+        if np.random.uniform() < epsilon:
+            act = np.random.randint(nA)
+        else:
+            act = np.argmax(Q[sta])
+        
+        for _ in range(max_steps):
+            nextsta, rew, ter, trunc, _ = env.step(act)
+            
+            if np.random.uniform() < epsilon:
+                nextact = np.random.randint(nA)
+            else:
+                nextact = np.argmax(Q[nextsta])
+            
+            td_err = rew + gamma * Q[nextsta, nextact] - Q[sta, act]
+            
+            E[sta, act] += 1
+            
+            Q += alpha * td_err * E
+            E *= gamma * lambtha
+            
+            sta, act = nextsta, nextact
+            
+            if ter or trunc:
+                break
+        
+        epsilon = max(min_epsilon, epsilon - epsilon_decay)
+                
+    return Q
