9c6a3bab64fecf5aef293b817d3ac31c939dbb14
ariel <ariel@gmail.com>
2025-11-14 22:31:11 -0300

    opo

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -14,3 +14,21 @@ def policy(matrix, weight):
     prob = expz / np.sum(expz, axis=1, keepdims=True)
 
     return prob
+
+def policy_gradient(state, weight):
+    """
+    safsafsafsa
+    """
+
+    state = state.reshape(1, -1)
+
+    probs = policy(state, weight)
+
+    act = np.random.choice(probs.shape[1], p=probs[0])
+
+    ones = np.zeros_like(probs)
+    ones[0, act] = 1
+
+    grad = state.T.dot(onest - probs)
+    
+    return act, grad
