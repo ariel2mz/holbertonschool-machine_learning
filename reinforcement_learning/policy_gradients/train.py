871ab07382171aecb62e4cc3a64d8feb9cac5127
ariel <ariel@gmail.com>
2025-11-14 23:16:10 -0300

    jk

---
+++ reinforcement_learning/policy_gradients/train.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+afsfsafsa
+"""
+import numpy as np
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+    """
+    asfgsasafsa
+    """
+
+    policy_gradient = __import__('policy_gradient').policy_gradient
+
+    acts = env.action_space.n
+    feats = env.observation_space.shape[0]
+    weight = np.random.rand(feats, acts)
+
+    scores = []
+
+    for ep in range(nb_episodes):
+        state, _ = env.reset()
+        eprew = []
+        done = False
+
+        while not done:
+
+            act, grad = policy_gradient(state, weight)
+
+            nextstate, reward, terminated, truncated, _ = env.step(act)
+            done = terminated or truncated
+            weight += alpha * grad * reward
+
+            eprew.append(reward)
+            state = nextstate
+
+        score = sum(eprew)
+        scores.append(score)
+
+        print("Episode: {} Score: {}".format(ep, score))
+    
+    return scores
