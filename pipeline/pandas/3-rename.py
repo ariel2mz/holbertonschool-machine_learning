4b6b1b64c329b0e57bd78cfccd266e5ba3387451
ariel <ariel@gmail.com>
2025-11-21 15:30:44 -0300

    fsafsa

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+asgsagsagsaa
+"""
+import pandas as pd
+
+
+def rename(df):
+    """
+    sfsafsafsa
+    """
+    df = df.rename(columns={"Timestamp": "Datetime"})
+    df["Datetime"] = pd.to_datetime(df["Datetime"])
+    df = df[["Datetime", "Close"]]
+
+    return df
