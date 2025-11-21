faa7ae1274080fa4773c049efba538189a2126ad
ariel <ariel@gmail.com>
2025-11-21 15:13:14 -0300

    safsafsa

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+asgsagsagsaa
+"""
+import pandas as pd
+import string
+
+def from_numpy(array):
+    """
+    sfasafsafsaf
+    """
+    cols = list(string.ascii_uppercase[:array.shape[1]])
+
+    return pd.DataFrame(array, columns=cols)
