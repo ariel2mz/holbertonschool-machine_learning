237442aa728741434c8119956689e197508c99d9
ariel <ariel@gmail.com>
2025-11-21 15:40:07 -0300

    sfazafsfsa

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+asgsagsagsaa
+"""
+import pandas as pd
+
+
+def array(df):
+    """
+    fafasfasfsa
+    """
+    selected = df[["High", "Close"]].tail(10)
+
+    arr = selected.to_numpy()
+    
+    return arr
