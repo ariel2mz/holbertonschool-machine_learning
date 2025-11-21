759db7ec0ffcc9b9aef5112bc358c230ba528192
ariel <ariel@gmail.com>
2025-11-21 15:25:43 -0300

    fghjklñ

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+asgsagsagsaa
+"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    afsafsafas
+    """
+    return pd.read_csv(filename, delimiter=delimiter)
