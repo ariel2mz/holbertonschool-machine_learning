7e04729bfcfd93e961f2657eed46e3a003ca43b1
ariel <ariel@gmail.com>
2025-11-28 20:43:57 -0300

    jkjk

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -11,25 +11,25 @@ def availableShips(passengerCount):
     """
     url = "https://swapi.dev/api/starships/"
     ships_list = []
-    
+
     while url:
         response = requests.get(url)
         data = response.json()
-        
+
         for ship in data['results']:
             passengers = ship.get('passengers', '0')
-            
+
             if passengers == 'unknown' or passengers == 'n/a' or not passengers:
                 continue
-            
+
             passengers = passengers.replace(',', '')
-            
+
             try:
                 if int(passengers) >= passengerCount:
                     ships_list.append(ship['name'])
             except ValueError:
                 continue
-        
+
         url = data.get('next')
-    
+
     return ships_list
