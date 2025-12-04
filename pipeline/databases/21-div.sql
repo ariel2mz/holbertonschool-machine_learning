bc8c13afb7940aac01bf7a5ae928f8a9f417fecc
ariel <ariel@gmail.com>
2025-12-04 19:55:10 -0300

    jmklklkl

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,17 @@
+-- asgsagsaghfsa
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END;
+//
+
+DELIMITER ;
