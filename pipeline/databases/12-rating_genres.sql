dbe6bacf15fd53c84adb53b54ab433c1704613ee
ariel <ariel@gmail.com>
2025-12-06 20:15:17 +0000

    sfasfsafsa

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,10 +1,7 @@
--- asfgasasafsaf
-SELECT tv_genres.name AS name,
-       SUM(tv_show_ratings.rating) AS rating
+-- safasfasfas
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-JOIN tv_show_genres
-     ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_show_ratings
-     ON tv_show_genres.show_id = tv_show_ratings.show_id
-GROUP BY tv_genres.id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id, tv_genres.name
 ORDER BY rating DESC;
--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,13 +1,13 @@
-DELIMITER //
+-- safsafsa
+DELIMITER $$
 
-CREATE TRIGGER decrease_quantity_after_order
+CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-END;
-//
+END$$
 
 DELIMITER ;
