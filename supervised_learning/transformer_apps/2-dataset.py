4b10cbe6f3b8c9c009f9dc177c16b420fa6f8194
ariel <ariel@gmail.com>
2025-12-07 14:51:57 -0300

    asfsa

--- supervised_learning/transformer_apps/2-dataset.py
+++ supervised_learning/transformer_apps/2-dataset.py
@@ -1,47 +1,26 @@
-#!/usr/bin/env python3
-"""
-qeweqwewq asdasd
-"""
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import transformers
-import numpy as np
-"""
-adsadsad asdasdas
-"""
 
 
 class Dataset:
-    """
-    asdsadsada
-    """
     def __init__(self):
-        """
-            Isadasd asdasdsa
-        """
         examples, _ = tfds.load(
             'ted_hrlr_translate/pt_to_en',
             as_supervised=True,
             with_info=True
         )
 
-        # Store original datasets
         self.raw_data_train = examples['train']
         self.raw_data_valid = examples['validation']
         
-        # Tokenize datasets
         a, b = self.tokenize_dataset(self.raw_data_train)
         self.tokenizer_pt, self.tokenizer_en = a, b
         
-        # Create tokenized datasets
         self.data_train = self.raw_data_train.map(self.tf_encode)
         self.data_valid = self.raw_data_valid.map(self.tf_encode)
 
     def tokenize_dataset(self, data):
-        """
-            adsasd sadasdsa
-        """
-
         abv = "neuralmind/bert-base-portuguese-cased"
         tpt = transformers.AutoTokenizer.from_pretrained(abv)
         ten = transformers.AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -66,34 +45,27 @@ class Dataset:
         return tpt, ten
 
     def encode(self, pt, en):
-        """
-        asffsafas
-        """
-
         pt_text = pt.numpy().decode('utf-8')
         en_text = en.numpy().decode('utf-8')
-
+        
         pt_tokens = self.tokenizer_pt.encode(pt_text)
         en_tokens = self.tokenizer_en.encode(en_text)
-
+        
         start_token = 2**13
         end_token = 2**13 + 1
+        
         pt_tokens = [start_token] + pt_tokens + [end_token]
         en_tokens = [start_token] + en_tokens + [end_token]
         
-        return np.array(pt_tokens), np.array(en_tokens)
+        return tf.convert_to_tensor(pt_tokens, dtype=tf.int64), tf.convert_to_tensor(en_tokens, dtype=tf.int64)
 
     def tf_encode(self, pt, en):
-        """
-        asfasfas
-        """
-
         pt_result, en_result = tf.py_function(
             func=self.encode,
             inp=[pt, en],
             Tout=[tf.int64, tf.int64]
         )
-
+        
         pt_result.set_shape([None])
         en_result.set_shape([None])
         
