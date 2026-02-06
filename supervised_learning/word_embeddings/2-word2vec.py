a7a603de4f06048e57305177562510453fce1142
ariel <ariel@gmail.com>
2026-02-06 03:40:31 -0300

    asa

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -38,4 +38,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         epochs=epochs
     )
 
+    model.build_vocab(sentences)
+    model.train(sentences, total_examples=model.corpus_count, epochs=epochs)
+
     return model
