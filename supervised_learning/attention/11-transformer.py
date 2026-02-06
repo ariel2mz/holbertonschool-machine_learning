1dc42931723ab3280c14cc6099c539773a70d9ce
ariel <ariel@gmail.com>
2026-02-06 12:25:23 -0300

    SAFASFAS

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -6,21 +6,23 @@ import tensorflow as tf
 Encoder = __import__('9-transformer_encoder').Encoder
 Decoder = __import__('10-transformer_decoder').Decoder
 
+
 class Transformer(tf.keras.Model):
     """
     asfasfsafasfsa
     """
     def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
                  max_seq_input, max_seq_target, drop_rate=0.1):
-                 """
-                 asfafasfasfsafas
-                 """
+        """
+        asfafasfasfsafas
+        """
         super().__init__()
         self.encoder = Encoder(N, dm, h, hidden, input_vocab, max_seq_input,
                                drop_rate)
         self.decoder = Decoder(N, dm, h, hidden, target_vocab, max_seq_target,
                                drop_rate)
-        self.linear = tf.keras.layers.Dense(units=target_vocab, activation=None)
+        self.linear = tf.keras.layers.Dense(units=target_vocab,
+                                            activation=None)
 
     def call(self, inputs, target, training, encoder_mask,
              look_ahead_mask, decoder_mask):
