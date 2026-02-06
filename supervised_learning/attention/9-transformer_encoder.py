d39763ad61b2b9ef6b988d8383fd6effbdafc533
ariel <ariel@gmail.com>
2026-02-06 12:02:33 -0300

    hkjjkkjk

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 """
-sadsadsa
+9-transformer_encoder.py
+Defines the Encoder class for a Transformer.
 """
+
 import tensorflow as tf
 import numpy as np
+
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
     """
-    sadsadsadsa
+    Encoder class for a Transformer.
     """
-    
+
     def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
         """
-        sadsadsadsad
+        Class constructor.
         """
         super(Encoder, self).__init__()
         self.N = N
@@ -23,22 +26,19 @@ class Encoder(tf.keras.layers.Layer):
 
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) 
-                      for _ in range(N)]
-
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
         """
-        asdasdsadsa
+        Forward pass of the encoder.
         """
-        seq_len = tf.shape(x)[1]
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        seq_len = tf.shape(x)[1]
         x += self.positional_encoding[:seq_len, :]
         x = self.dropout(x, training=training)
+        for block in self.blocks:
+            x = block(x, training, mask)
 
-        for i in range(self.N):
-            x = self.blocks[i](x, training, mask)
-        
         return x
