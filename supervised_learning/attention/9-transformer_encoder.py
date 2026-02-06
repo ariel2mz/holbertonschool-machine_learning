f44479e5122fd8571415ca7b451fb86229cea998
ariel <ariel@gmail.com>
2026-02-06 12:09:03 -0300

    safsa

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -5,34 +5,24 @@ Defines the Encoder class for a Transformer.
 """
 
 import tensorflow as tf
-import numpy as np
 
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
 
 
 class Encoder(tf.keras.layers.Layer):
-    """
-    Encoder class for a Transformer.
-    """
-
-    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
-        """
-        Class constructor.
-        """
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
         super(Encoder, self).__init__()
         self.N = N
         self.dm = dm
-
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, mask):
-        """
-        Forward pass of the encoder.
-        """
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
         seq_len = tf.shape(x)[1]
@@ -40,5 +30,4 @@ class Encoder(tf.keras.layers.Layer):
         x = self.dropout(x, training=training)
         for block in self.blocks:
             x = block(x, training, mask)
-
         return x
