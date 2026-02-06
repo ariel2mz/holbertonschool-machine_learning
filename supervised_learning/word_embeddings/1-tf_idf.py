fc5c656606e16a81f07d6e28b34d95eb4394ef76
ariel <ariel@gmail.com>
2026-02-06 03:30:29 -0300

    asdasdas

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -9,8 +9,8 @@ import math
 
 def tf_idf(sentences, vocab=None):
     """
-    asfasfsafsa
-    safasfsaf
+    asfasfsafas
+    fasfasfsa
     """
     processed = []
     for sent in sentences:
@@ -27,37 +27,33 @@ def tf_idf(sentences, vocab=None):
         vocab = sorted(set(all_words))
 
     features = list(vocab)
-
     s = len(sentences)
     f = len(features)
 
-    tf_matrix = np.zeros((s, f), dtype=float)
+    tf = np.zeros((s, f), dtype=float)
     df = np.zeros(f, dtype=int)
 
     feature_to_index = {word: i for i, word in enumerate(features)}
 
     for i, tokens in enumerate(processed):
-        word_count = {}
         for token in tokens:
             if token in feature_to_index:
-                word_count[token] = word_count.get(token, 0) + 1
-
-        for word, count in word_count.items():
-            tf_matrix[i, feature_to_index[word]] = count / len(tokens)
+                tf[i, feature_to_index[token]] += 1
 
     for j in range(f):
         for i in range(s):
-            if tf_matrix[i, j] > 0:
+            if tf[i, j] > 0:
                 df[j] += 1
 
-    idf = np.zeros(f, dtype=float)
+    idf = np.zeros(f)
     for j in range(f):
         if df[j] > 0:
             idf[j] = math.log(s / df[j])
 
-    tf_idf_matrix = np.zeros((s, f), dtype=float)
+    tf_idf_matrix = tf * idf
     for i in range(s):
-        for j in range(f):
-            tf_idf_matrix[i, j] = tf_matrix[i, j] * idf[j]
+        norm = np.linalg.norm(tf_idf_matrix[i])
+        if norm > 0:
+            tf_idf_matrix[i] /= norm
 
     return tf_idf_matrix, features
