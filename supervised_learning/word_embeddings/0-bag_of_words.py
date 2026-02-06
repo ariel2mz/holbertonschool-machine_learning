41ded0682a0989eb1635ac95298ade4cb284da2b
ariel <ariel@gmail.com>
2026-02-06 03:18:23 -0300

    asa

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -8,41 +8,41 @@ import string
 def bag_of_words(sentences, vocab=None):
     """
     Creates a bag of words embedding matrix
-
+    
     Args:
         sentences: list of sentences to analyze
         vocab: list of vocabulary words to use for analysis
                If None, all words within sentences should be used
-
+    
     Returns:
         embeddings: numpy.ndarray of shape (s, f)
         features: list of the features used for embeddings
     """
     processed = []
-    for sent in sentences:
-        sent = sent.lower()
-        sent = sent.replace("'s", "")
-        sent = sent.translate(str.maketrans('', '', string.punctuation))
-        tokens = sent.split()
+    for sentence in sentences:
+        sentence = sentence.lower()
+        sentence = sentence.replace("'s", "")
+        sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+        tokens = sentence.split()
         processed.append(tokens)
-
+    
     if vocab is None:
         all_words = []
         for tokens in processed:
             all_words.extend(tokens)
         vocab = sorted(set(all_words))
-
-    features = list(vocab)
-
+    
+    features = np.array(sorted(vocab))
+    
     s = len(sentences)
     f = len(features)
-
+    
     embeddings = np.zeros((s, f), dtype=int)
     feature_to_index = {word: i for i, word in enumerate(features)}
-
+    
     for i, tokens in enumerate(processed):
         for token in tokens:
             if token in feature_to_index:
                 embeddings[i, feature_to_index[token]] += 1
-
+    
     return embeddings, features
